38ebac52e78df00ad1a54d179539378da4cf7773
Ryoh Kunihiro <11649545+rkunihiro@users.noreply.github.com>
2020-07-09 22:47:44 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CSharpSB
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+# C sharp sandbox
+
+## Reference
+- https://docs.microsoft.com/ja-jp/dotnet/csharp/language-reference/
+
+## IDE
+- Visual Studio
+  https://visualstudio.microsoft.com/ja/vs/
